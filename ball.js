767e293d5a603b32c2e279dc3f3f18911e516120
sanjana3-14 <73714604+sanjana3-14@users.noreply.github.com>
2021-01-27 18:04:16 +0000

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,25 @@
+class Ball{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.5,
+            'friction':0.3,
+            'density':2
+        }
+        this.body = Bodies.circle(x, y, 20,options);
+        this.radius = 40;
+        this.image = loadImage("paper.png")
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        //ellipseMode(CENTER);
+        fill("grey")
+       // ellipse( 0, 0, this.radius,this.radius);
+       imageMode(CENTER) 
+       image(this.image,0,0,this.radius,this.radius)
+       pop();
+      }
+}
